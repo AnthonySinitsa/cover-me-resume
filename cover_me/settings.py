d3f46967bcc042608a34ec814ef45236112e53ac
AnthonySinitsa <itonyemail@gmail.com>
2023-09-05 01:20:19 +0000

    base html

--- cover_me/settings.py
+++ cover_me/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'cover_me.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -130,3 +130,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
