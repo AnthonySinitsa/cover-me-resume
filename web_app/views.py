288fcfd0e8c60dc38cf5eba6f47c2f2d4c8894a7
AnthonySinitsa <itonyemail@gmail.com>
2023-09-07 23:57:25 +0000

    pdf reader and extracting text

--- web_app/views.py
+++ web_app/views.py
@@ -1,10 +1,14 @@
+import json
+import openai
+import PyPDF2
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 from django.shortcuts import render, redirect
 from .forms import UserRegisterForm, ResumeUploadForm
 from django.contrib.auth.decorators import login_required
 from .models import Resume
-import json
+from django.http import JsonResponse
+from django.conf import settings
 
 class HomePageView(LoginRequiredMixin, TemplateView):
   template_name = 'home.html'
@@ -69,3 +73,15 @@ def job_results(request):
 
   # Step 3: Passing the Data to the Template
   return render(request, 'job_results.html', {'jobs': jobs_list})
+
+def extract_text_from_pdf(file_path):
+  with open(file_path, 'rb') as file:
+    reader = PyPDF2.PdfFileReader(file)
+    text = ""
+    for page_num in range(reader.numPages):
+      text += reader.getPage(page_num).extractText()
+  return text
+
+def get_user_resume_as_text(user):
+    resume = user.resume_set.latest('uploaded_at')  # 'uploaded_at' is a DateTimeField in your Resume model
+    return extract_text_from_pdf(resume.resume_file.path)
