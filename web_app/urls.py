b7b94e9d02b8997d7a953132494fd6045cd4da6e
AnthonySinitsa <itonyemail@gmail.com>
2023-09-08 00:34:26 +0000

    generate_cover_letter button added and path added

--- web_app/urls.py
+++ web_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
   path('profile/', views.profile, name='profile'),
   path('job-search/', views.job_search, name='job_search'),
   path('job-results/', views.job_results, name='job_results'),
+  path('generate_cover_letter/<int:job_index>/', views.generate_cover_letter, name='generate_cover_letter'),
 ]
