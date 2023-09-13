1c338cdaa2246e9626fab45d3ef22c15ba8ae54e
AnthonySinitsa <itonyemail@gmail.com>
2023-09-13 20:11:03 +0000

    updated tasks.py

--- web_app/tasks.py
+++ web_app/tasks.py
@@ -1,21 +1,35 @@
 from celery import shared_task
 import subprocess
-import os
+from django.conf import settings
 
 @shared_task
 def run_scraper(job_title, job_location):
   # Set environment variables
-  os.environ["SCRAPFLY_KEY"] = "your_key_here"
-  os.environ["PYTHONPATH"] = "/home/anton/projects/cover-me-resume"
+  commands = [
+    f"export SCRAPFLY_KEY='{settings.SCRAPFLY_KEY}'",
+    f"export PYTHONPATH=/home/anton/projects/cover-me-resume:$PYTHONPATH",
+    "poetry run python web_app/scrapers/indeed_scraper/run.py"
+  ]
 
-  # Define the command to run the scraper
-  cmd = ["poetry", "run", "python", "web_app/scrapers/indeed_scraper/run.py"]
+  # Join the commands with '&&' to run them consecutively
+  command = ' && '.join(commands)
 
-  # Execute the command
-  try:
-    subprocess.check_call(cmd)
-    # You can add more logic here to handle the output if needed.
-    return "Scraper ran successfully!"
-  except subprocess.CalledProcessError as e:
-    # Handle errors related to the subprocess call
-    return f"Scraper failed with error: {e}"
+  # Use subprocess to execute the command
+  process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+  stdout, stderr = process.communicate()
+
+  if process.returncode != 0:
+    # If there's an error, you can raise an exception or handle it appropriately
+    raise Exception(f"Scraper failed with error: {stderr.decode('utf-8')}")
+
+  return stdout.decode('utf-8')
+
+
+# project_root = Path(__file__).resolve().parents[3]  # Adjust the number based on your directory structure
+
+#     # Define the commands to run the scraper
+#     commands = [
+#         f"export SCRAPFLY_KEY='{settings.SCRAPFLY_KEY}'",
+#         f"export PYTHONPATH={project_root}:$PYTHONPATH",
+#         "poetry run python web_app/scrapers/indeed_scraper/run.py"
+#     ]
