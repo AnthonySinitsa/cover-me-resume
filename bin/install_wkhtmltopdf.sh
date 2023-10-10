550b5ccb5e0cd5e8f8a508ab8d540c019438023e
AnthonySinitsa <itonyemail@gmail.com>
2023-10-10 04:32:57 +0000

    script changes

--- bin/install_wkhtmltopdf.sh
+++ bin/install_wkhtmltopdf.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
+echo "-----> Installing wkhtmltopdf"
 
-WKHTMLTOPDF_VERSION='0.12.6.1-2'
-WKHTMLTOPDF_DEB="wkhtmltox_${WKHTMLTOPDF_VERSION}.jammy_amd64.deb"
-WKHTMLTOPDF_URL="https://github.com/wkhtmltopdf/packaging/releases/download/${WKHTMLTOPDF_VERSION}/${WKHTMLTOPDF_DEB}"
+# Download wkhtmltopdf package
+wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6.1-2/wkhtmltox_0.12.6.1-2.jammy_amd64.deb
 
-echo "Installing wkhtmltopdf..."
-curl -L $WKHTMLTOPDF_URL -o $WKHTMLTOPDF_DEB
-dpkg -i $WKHTMLTOPDF_DEB
-apt install -f -y
-rm $WKHTMLTOPDF_DEB
+# Install wkhtmltopdf package
+dpkg -i wkhtmltox_0.12.6.1-2.jammy_amd64.deb || true
+
+# Move binaries to the bin directory
+mv /usr/local/bin/wkhtmltopdf /app/bin
+mv /usr/local/bin/wkhtmltoimage /app/bin
+
+echo "-----> wkhtmltopdf Installation done"
