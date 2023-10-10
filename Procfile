6e9c25dc6474c98d26e256bd082c0e2befe87f8b
AnthonySinitsa <itonyemail@gmail.com>
2023-10-10 04:19:53 +0000

    updates

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
+release: sh /app/bin/install_wkhtmltopdf.sh
 web: gunicorn cover_me.wsgi
 worker: celery -A cover_me worker -l info
--- bin/install_wkhtmltopdf.sh
+++ bin/install_wkhtmltopdf.sh
@@ -1,7 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
-WKHTMLTOPDF_URL="https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6.1-2/wkhtmltox_0.12.6.1-2.jammy_amd64.deb"
+WKHTMLTOPDF_VERSION='0.12.6.1-2'
+WKHTMLTOPDF_DEB="wkhtmltox_${WKHTMLTOPDF_VERSION}.jammy_amd64.deb"
+WKHTMLTOPDF_URL="https://github.com/wkhtmltopdf/packaging/releases/download/${WKHTMLTOPDF_VERSION}/${WKHTMLTOPDF_DEB}"
 
-echo "-----> Installing wkhtmltopdf"
-curl -L $WKHTMLTOPDF_URL -o wkhtmltopdf.deb
-sudo dpkg -i wkhtmltopdf.deb
+echo "Installing wkhtmltopdf..."
+curl -L $WKHTMLTOPDF_URL -o $WKHTMLTOPDF_DEB
+dpkg -i $WKHTMLTOPDF_DEB
+apt install -f -y
+rm $WKHTMLTOPDF_DEB
