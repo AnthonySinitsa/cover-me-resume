270fb52f639db89685db2345bc7757170fb5f5e5
AnthonySinitsa <itonyemail@gmail.com>
2023-10-10 05:17:55 +0000

    script

--- bin/install_wkhtmltopdf.sh
+++ bin/install_wkhtmltopdf.sh
@@ -2,7 +2,7 @@
 echo "-----> Installing wkhtmltopdf"
 
 # Download wkhtmltopdf package
-wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6.1-2/wkhtmltox_0.12.6.1-2.jammy_amd64.deb
+wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.focal_amd64.deb
 
 # Create a temporary directory
 mkdir /app/tmp
