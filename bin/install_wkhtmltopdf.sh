e51d9b255bca3e064ee8d63678c38ee03972fac9
AnthonySinitsa <itonyemail@gmail.com>
2023-10-10 04:47:51 +0000

    script

--- bin/install_wkhtmltopdf.sh
+++ bin/install_wkhtmltopdf.sh
@@ -10,9 +10,17 @@ mkdir /app/tmp
 # Extract the Debian package to the temporary directory
 dpkg-deb -x wkhtmltox_0.12.6.1-2.jammy_amd64.deb /app/tmp
 
+# Print out extracted files
+echo "-----> Extracted files:"
+ls /app/tmp/usr/local/bin
+
 # Move binaries to the bin directory
 mv /app/tmp/usr/local/bin/wkhtmltopdf /app/bin
 mv /app/tmp/usr/local/bin/wkhtmltoimage /app/bin
+
+# Print out bin directory contents
+echo "-----> Bin directory contents:"
+ls /app/bin
 
 # Clean up the temporary directory
 rm -rf /app/tmp
