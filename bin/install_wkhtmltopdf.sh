f4ff492202cecb95be7c3dd1f7a973092a233ff3
AnthonySinitsa <itonyemail@gmail.com>
2023-10-10 04:39:31 +0000

    script changes

--- bin/install_wkhtmltopdf.sh
+++ bin/install_wkhtmltopdf.sh
@@ -4,11 +4,17 @@ echo "-----> Installing wkhtmltopdf"
 # Download wkhtmltopdf package
 wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6.1-2/wkhtmltox_0.12.6.1-2.jammy_amd64.deb
 
-# Install wkhtmltopdf package
-dpkg -i wkhtmltox_0.12.6.1-2.jammy_amd64.deb || true
+# Create a temporary directory
+mkdir /app/tmp
+
+# Extract the Debian package to the temporary directory
+dpkg-deb -x wkhtmltox_0.12.6.1-2.jammy_amd64.deb /app/tmp
 
 # Move binaries to the bin directory
-mv /usr/local/bin/wkhtmltopdf /app/bin
-mv /usr/local/bin/wkhtmltoimage /app/bin
+mv /app/tmp/usr/local/bin/wkhtmltopdf /app/bin
+mv /app/tmp/usr/local/bin/wkhtmltoimage /app/bin
+
+# Clean up the temporary directory
+rm -rf /app/tmp
 
 echo "-----> wkhtmltopdf Installation done"
