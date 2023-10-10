382f25de716b769169716f554fe58c4cba6a1b0b
AnthonySinitsa <itonyemail@gmail.com>
2023-10-10 05:23:08 +0000

    script

--- bin/install_wkhtmltopdf.sh
+++ bin/install_wkhtmltopdf.sh
@@ -1,28 +1,13 @@
 #!/bin/sh
 echo "-----> Installing wkhtmltopdf"
 
-# Download wkhtmltopdf package
-wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.focal_amd64.deb
+# Assuming the wkhtmltopdf binary is included in the project repository root
+# Copy wkhtmltopdf binary to the bin directory
+cp wkhtmltopdf /app/bin/
+chmod +x /app/bin/wkhtmltopdf  # Make sure it is executable
 
-# Create a temporary directory
-mkdir /app/tmp
-
-# Extract the Debian package to the temporary directory
-dpkg-deb -x wkhtmltox_0.12.6.1-2.jammy_amd64.deb /app/tmp
-
-# Print out extracted files
-echo "-----> Extracted files:"
-ls /app/tmp/usr/local/bin
-
-# Move binaries to the bin directory
-mv /app/tmp/usr/local/bin/wkhtmltopdf /app/bin
-mv /app/tmp/usr/local/bin/wkhtmltoimage /app/bin
-
-# Print out bin directory contents
-echo "-----> Bin directory contents:"
-ls /app/bin
-
-# Clean up the temporary directory
-rm -rf /app/tmp
+echo "wkhtmltopdf installed at:"
+ls -alh /app/bin/wkhtmltopdf  # This will confirm that the file is present and executable
 
 echo "-----> wkhtmltopdf Installation done"
+
